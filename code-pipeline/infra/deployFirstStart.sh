a2b98652301c4b650f1a06aa956c39372da5326d
francoPersonal <jafr6@hotmail.com>
2021-03-03 12:30:30 -0600

    clean deploy

--- code-pipeline/infra/deployFirstStart.sh
+++ code-pipeline/infra/deployFirstStart.sh
@@ -1,4 +1,5 @@
 # npm i -g serverless to update 
 npm install serverless-s3-remover
 npm i serverless-pseudo-parameters
+npm install serverless-deployment-bucket
 serverless deploy --env dev --token-value test
